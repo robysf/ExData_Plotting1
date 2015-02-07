0b19befe85f8fbcd4af7b4b70474013f28371735
robysf <robysf@users.noreply.github.com>
2015-02-07 12:40:56 -0500

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,26 @@
+#######################################
+###### EXPLORATORY DATA ANALYSIS ######
+############  Assignment 1 ############
+############### Plot 1  ###############
+#######################################
+
+# Read in data (must be run from directory with "household_power_consumption.txt")
+setwd("/Users/robysf/Documents/Fellowship/Coursera/Exploratory_Data_Analysis/Project1/")
+powerData <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, stringsAsFactors = FALSE)
+
+# convert plotted parameter to numeric from character
+as.numeric(powerData$Global_active_power)
+
+# subset data to include only Feb 1 and Feb 2, 2007
+as.Date(powerData$Date, format = "%d/%m/%Y")
+install.packages("dplyr")
+library(dplyr)
+plot1Data <- filter(powerData, Date == "2007-02-01" | Date == "2007-02-02")
+
+# generate plot in local display
+hist(powerDataFeb1Feb2$Global_active_power, col = "red", 
+     xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+
+# save as png file (default appears to be 480 x 480)
+dev.copy(png, file = "plot1.png")
+dev.off()
