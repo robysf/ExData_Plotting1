24a713765429bb96049002b2aa5a1fc886e587f2
robysf <robysf@users.noreply.github.com>
2015-02-07 19:00:30 -0500

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -15,7 +15,7 @@ powerData$Sub_metering_2 <- as.numeric(powerData$Sub_metering_2)
 
 # subset data to include only Feb 1 and Feb 2, 2007
 powerData$Date <- as.Date(powerData$Date, format = "%d/%m/%Y")
-install.packages("dplyr")
+if(!require(dplyr)) install.packages("dplyr")
 library(dplyr)
 plotData <- filter(powerData, Date == "2007-02-01" | Date == "2007-02-02")
 
